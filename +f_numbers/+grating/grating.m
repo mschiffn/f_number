0df9c47129a5d48f480241253dd0b4d150a93959
Martin F. Schiffner <martin.schiffner@rub.de>
2022-01-16 01:00:39 +0100

    +f_numbers/+grating: - fixed wrong type of property anti_aliasing_tf in class grating - uncommented missing computation of property lower_bound

--- +f_numbers/+grating/grating.m
+++ +f_numbers/+grating/grating.m
@@ -7,23 +7,23 @@
 %
 classdef (Abstract) grating < f_numbers.f_number
 
-	%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-	%% properties
-	%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-	properties (SetAccess = private)
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %% properties
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    properties (SetAccess = private)
 
         % independent properties
         angle_lb_deg ( 1, 1 ) double { mustBeNonempty, mustBeNonnegative, mustBeLessThanOrEqual( angle_lb_deg, 90 ) } = 45 % lower bound on the angular distances of the first-order grating lobes (degree)
         F_number_ub ( 1, 1 ) double { mustBeNonempty, mustBeNonnegative } = 3 % upper bound on the F-number
-        anti_aliasing_tf ( 1, 1 ) boolean { mustBeNonempty, mustBeNonnegative } = true % include anti-aliasing filter
+        anti_aliasing_tf ( 1, 1 ) logical { mustBeNonempty, mustBeNonnegative } = true % include anti-aliasing filter
 
         % dependent properties
-        thresh ( 1, 1 ) double { mustBeNonnegative, mustBeNonempty } = sin( 40 * pi / 180 )	% threshold resulting from lower bound on the first-order grating lobe angle
-        lower_bound ( 1, 1 ) double { mustBeNonnegative, mustBeNonempty } = 1 / ( 1 + sin( 40 * pi / 180 ) )
-        upper_bound ( 1, 1 ) double { mustBeNonnegative, mustBeNonempty } = 1 / ( 1 / sqrt( 1 + ( 2 * 3 )^2 ) + sin( 40 * pi / 180 ) )
-        critical ( 1, 1 ) double { mustBeNonnegative, mustBeNonempty } = 1 / sin( 40 * pi / 180 )
+        thresh ( 1, 1 ) double { mustBeNonnegative, mustBeNonempty } = sin( deg2rad( 45 ) )	% threshold resulting from lower bound on the first-order grating lobe angle
+        lower_bound ( 1, 1 ) double { mustBeNonnegative, mustBeNonempty } = 1 / ( 1 + sin( deg2rad( 45 ) ) )
+        upper_bound ( 1, 1 ) double { mustBeNonnegative, mustBeNonempty } = 1 / ( 1 / sqrt( 1 + ( 2 * 3 )^2 ) + sin( deg2rad( 45 ) ) )
+        critical ( 1, 1 ) double { mustBeNonnegative, mustBeNonempty } = 1 / sin( deg2rad( 45 ) )
 
-	end % properties
+    end % properties
 
 	%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 	%% methods
@@ -66,14 +66,14 @@ classdef (Abstract) grating < f_numbers.f_number
                 % set independent properties
                 objects( index_object ).angle_lb_deg = angles_lb_deg( index_object );
                 objects( index_object ).F_number_ub = F_numbers_ub( index_object );
-%                 objects( index_object ).anti_aliasing = anti_aliasing_tfs( index_object );
+                objects( index_object ).anti_aliasing_tf = anti_aliasing_tfs( index_object );
 
                 % set dependent properties
-                objects( index_object ).thresh = sin( objects( index_object ).angle_lb_deg * pi / 180 );
+                objects( index_object ).thresh = sin( deg2rad( objects( index_object ).angle_lb_deg ) );
 %                 if objects( index_object ).anti_aliasing
 %                     objects( index_object ).lower_bound = 1 / ( 2 * objects( index_object ).thresh );
 %                 else
-%                     objects( index_object ).lower_bound = 1 / ( 1 + objects( index_object ).thresh );
+                    objects( index_object ).lower_bound = 1 / ( 1 + objects( index_object ).thresh );
 %                 end
                 objects( index_object ).upper_bound = 1 / ( 1 / sqrt( 1 + ( 2 * objects( index_object ).F_number_ub )^2 ) + objects( index_object ).thresh );
                 objects( index_object ).critical = 1 / objects( index_object ).thresh;
